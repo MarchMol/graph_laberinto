dd0fb6e36afab95b9d70b2897bc6615f323a3cdb
MarchMol <marchmol98@gmail.com>
2024-08-03 12:13:58 -0600

    feat: working laberynth/ done in class

---
+++ src/player.rs
@@ -0,0 +1,45 @@
+use minifb::Window;
+use nalgebra_glm::{pi, Vec2};
+use core::f32::consts::PI;
+
+const MOVE_SPEED: f32 = 10.0;
+const ROTATION_SPEED: f32 = PI/30.0;
+
+pub struct Player{
+    pub pos:nalgebra_glm::Vec2,
+    pub a: f32,
+    pub fov: f32
+}
+
+impl Player{
+    pub fn new()->Self{
+        Player{
+            pos: Vec2::new(0.0, 0.0),
+            a: PI/3.0,
+            fov: PI/3.0
+        }
+    }
+
+    pub fn setPos(&mut self, x: f32, y: f32){
+        self.pos.x = x;
+        self.pos.y = y;
+    }
+    pub fn incA(&mut self, orientation: bool){ // true - Right, false - left
+        if orientation{
+            self.a += ROTATION_SPEED;
+        } else {
+            self.a -= ROTATION_SPEED;
+        }
+    }
+    pub fn incPos(&mut self, orientation: bool){ // true - front, false - back
+        let cos = MOVE_SPEED*self.a.cos();
+        let sin = MOVE_SPEED*self.a.sin();
+        if orientation{
+            self.pos.x += cos;
+            self.pos.y += sin;
+        } else {
+            self.pos.x -= cos;
+            self.pos.y -= sin;
+        }
+    }
+}
